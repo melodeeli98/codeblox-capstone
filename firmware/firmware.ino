98669b16f2956fd82feb68d9254d0562b5e7a1b7
Eric Maynard <ermay12@gmail.com>
2020-04-22 01:05:40 -0400

    made master a tile

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,22 +1,30 @@
 #include "codeblox_driver.h"
 
+byte my_sensors;
+
+int stopped;
+
 void handleNewMessage(const Message& message, enum Side_Name side) {
   LOG(String("NM ") + sideToString(side) + String(": ") + message.toString());
   switch (message.type) {
     case Message_Type::stop:
+      stopped++;
+      if(stopped == 4){
+        Serial.println("0,-1:" + String(my_sensors));
+        Serial.println("done");
+      }
       break;
     case Message_Type::done:
       LOG("DONE!");
       stopComm(side);
-      Serial.println("done");
       break;
     case Message_Type::parent:
       stopComm(side);
       break;
     case Message_Type::tile:
       {
-        int x = (int) message.words[1];
-        int y = (int) message.words[2];
+        int x = (int)((char) message.words[1]);
+        int y = (int)((char) message.words[2]);
         byte encoding = message.words[3];
         Serial.println(String(x) + "," + String(y) + ":" + String(encoding));
       }
@@ -30,6 +38,8 @@ void handleNewMessage(const Message& message, enum Side_Name side) {
 void processSerialMessage(char *message)
 {
   if (String(message) == "start") {
+    readReflectiveSensorsLater(&my_sensors);
+    stopped = 0;
     LOG("Starting");
     startCommAllSides();
     Message parentMessage (Message_Type::parent, Side_Name::top);
